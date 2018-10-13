932a6c171e512bb86051c1fa793606e9c079e45d
Brian Lee <blee025432@gmail.com>
2018-10-13 00:11:40 -0400

    pandas?

--- read.py
+++ read.py
@@ -1,11 +1,22 @@
 import json
+import ijson
 from pprint import pprint
+import pandas as pd
 
 def file_read(url):
-	with open(url) as f:
+	with open(url,'r') as f:
 		data = json.load(f)
 	pprint(data)
 	print('a')
 
+def file_read_ijson(url):
+	with open(url,'r') as f:
+		objects = ijson.items(f,'geometry.coordinates.items')
+		columns=list(objects)
+
+def file_read_pandas(url):
+	data_df = pd.read_json(url,lines=True)
+	print(data_df)
 if __name__ == "__main__":
-	file_read('/run/media/brianl/SAMSUNG USB/RideCommandForHack_example.json')
+	file_read_pandas('/run/media/brianl/SAMSUNG USB/RideCommandForHack_example.json')
+
