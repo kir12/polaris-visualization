67995e417d4b955316547c3e6b68af4eaec54994
Brian Lee <blee025432@gmail.com>
2018-10-13 02:28:53 -0400

    example handling of data_df

--- read.py
+++ read.py
@@ -1,7 +1,8 @@
 import json
 import ijson
-from pprint import pprint
 import pandas as pd
+import matplotlib.pyplot as plt
+from pprint import pprint
 
 def file_read(url):
 	with open(url,'r') as f:
@@ -15,8 +16,20 @@ def file_read_ijson(url):
 		columns=list(objects)
 
 def file_read_pandas(url):
+
+	#opens JSON in pandas
+	#data_df is of type series
 	data_df = pd.read_json(url,lines=True)
-	print(data_df)
+
+	#get all 1st all attributes
+	print(data_df.columns)
+
+	#example of calling series 1st level attribute
+	print(data_df.creationDate)
+
+	#ietereates through the Property series. Each 'test' is a Python dictionary
+	for test in data_df.properties:
+		print(test.keys())
 if __name__ == "__main__":
 	file_read_pandas('/run/media/brianl/SAMSUNG USB/RideCommandForHack_example.json')
 
