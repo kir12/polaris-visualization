367ed27497605cb5ed18462be1ba5303d90185ec
Brian Lee <blee025432@gmail.com>
2018-10-13 13:03:34 -0400

    properly scalead maps

--- polaris/world/views.py
+++ polaris/world/views.py
@@ -4,6 +4,11 @@ from world.read import file_read_pandas, pandas_points
 
 def index(request):
 	coords = pandas_points('/run/media/brianl/SAMSUNG USB/RideCommandForHack_example.json')
-	print(coords)
-	return render(request,'world/map.html',{'coords':coords})
+	#print(coords)
+	sum_long = 0
+	sum_lat = 0
+	for coord in coords:
+		sum_long+=coord[0]
+		sum_lat+=coord[1]
+	return render(request,'world/map.html',{'coords':coords,'avg_long':sum_long/len(coords),'avg_lat':sum_lat/len(coords)})
 # Create your views here.
