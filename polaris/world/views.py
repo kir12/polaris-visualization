f4ccb3d26874943b5c7628ddd7a61302831888ef
Brian Lee <blee025432@gmail.com>
2018-10-13 11:44:48 -0400

    basic tempalte system world

--- polaris/world/views.py
+++ polaris/world/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse('polaris')
+	return render(request,'world/map.html',{})
 # Create your views here.
