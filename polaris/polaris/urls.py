897e9fc4de0ab988efa1db5f0c9044757e8943cb
Brian Lee <blee025432@gmail.com>
2018-10-14 04:42:48 -0400

    missed an S

--- polaris/polaris/urls.py
+++ polaris/polaris/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from polaris import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('world/',include('world.urls'))
-]
+	path('world/',include('world.urls')),
+	path('',views.home,name='home')
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
