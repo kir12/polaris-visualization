53e53db6bbce4d397d1cd7e93dc554d177ab0451
Brian Lee <blee025432@gmail.com>
2018-10-13 23:15:37 -0400

    map view set

--- polaris/world/urls.py
+++ polaris/world/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.index,name='index')
+	path('',views.index,name='index'),
+	path('world_view/',views.world_view,name='world_view')
 ]
--- polaris/world/views.py
+++ polaris/world/views.py
@@ -6,21 +6,49 @@ import pandas as pd
 from dateparser import parse
 import datetime
 
+#this breaks with the big file
+#moved above as a global var
+data_df = pd.read_json('/run/media/brianl/SAMSUNG USB/RideCommandForHack_example.json',lines=True)
+print('json file loaded')
+
+
 def index(request):
 
 	#opens the JSON files, extracts a random point, and extacts the lat/long coords, time/distance, and start-end timestamp
-	data_df = pd.read_json('/run/media/brianl/SAMSUNG USB/RideCommandForHack_example.json',lines=True)
+	print('page loading started')
 	datapoint = data_df.iloc[randint(0,len(data_df))]
+	print('specific data point isolated')
+
 	properties = datapoint.properties
+	print('properties apect extracted')
+
 	side_values = {'totalDurationInSeconds':int(properties['totalDurationInSeconds']),'totalDistanceInMeters':round(properties['totalDistanceInMeters'],2),'startTimestamp':parse(str(properties['startTimestamp']['$date'])),'endTimestamp':parse(str(properties['endTimestamp']['$date']))}
-	#print(side_values)
+	print('side_values dictionary created')
+
 	coords = datapoint.geometry['coordinates']
+	print('coordinates list extracted')
 
 	sum_long = 0
 	sum_lat = 0
 	for coord in coords:
 		sum_long+=coord[0]
 		sum_lat+=coord[1]
+	print('average coords calculated')
 
 	return render(request,'world/map.html',{'coords':coords,'avg_long':sum_long/len(coords),'avg_lat':sum_lat/len(coords),'side_values':side_values})
 # Create your views here.
+
+def world_view(request):
+	average_points = []
+	print(data_df.columns)
+	for datapoint in data_df.geometry:
+		print(type(datapoint))
+		sum_long =0
+		sum_lat = 0
+		for coord in datapoint['coordinates']:
+			sum_long+=coord[0]
+			sum_lat+=coord[1]
+		average_points.append([sum_long/len(datapoint['coordinates']),sum_lat/len(datapoint['coordinates'])])
+	print(len(average_points))
+	return render(request,'world/world_view.html',{'average_points':average_points})
+
