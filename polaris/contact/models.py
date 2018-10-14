4c4a31be5aed8bb79c1ee98b72b1b531a72c8135
Brian Lee <blee025432@gmail.com>
2018-10-14 10:10:26 -0400

    created contact db

--- polaris/contact/models.py
+++ polaris/contact/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Contact(models.Model):
+	name = models.CharField(max_length=200)
+	email = models.EmailField()
+	message=models.TextField()
+
 # Create your models here.
