c49fcd6a66d97ac642eda37989e50a333a0a7047
Brian Lee <blee025432@gmail.com>
2018-10-14 09:14:39 -0400

    in progress of implementing django template

--- polaris/polaris/views.py
+++ polaris/polaris/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-	return render(request,'polaris/index.html')
+	return render(request,'polaris/home.html')
 
